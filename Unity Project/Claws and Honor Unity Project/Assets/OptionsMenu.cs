aa778fd710b76ce0321b5b09f39f73d35cb99ba0
GiveMeButter <33163518+GiveMeButter@users.noreply.github.com>
2018-03-20 10:52:32 -0600

    MasterMixer

--- Unity Project/Claws and Honor Unity Project/Assets/OptionsMenu.cs
+++ Unity Project/Claws and Honor Unity Project/Assets/OptionsMenu.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AudioMixer audioMixer;
+
+	public void SetMaster (float master)
+    {
+        audioMixer.SetFloat("master", master);
+    }
 }
