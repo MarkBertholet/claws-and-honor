c21514cc5e6e35c90e3a1ecca1d9f4b14f6809c2
DragonGeddon <starcannon237@gmail.com>
2018-03-30 10:39:39 -0600

    changed movement controls

---
+++ Unity Project/Claws and Honor Unity Project/Assets/Scripts/Saving and loading/GameControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;  
+
+    public float health;
+    public float experience;
+    
+	// Use this for initialization
+	void Awake () {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Health: " + health);
+        GUI.Label(new Rect(10, 40, 100, 30), "Experience: " + experience);
+    }
+}
