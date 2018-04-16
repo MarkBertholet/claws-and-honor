46e93c912cffaa121215d694631580f5296c4224
DragonGeddon <starcannon237@gmail.com>
2018-04-16 10:26:09 -0600

    the forest area

---
+++ Unity Project/Claws and Honor Unity Project/Assets/Scripts/Player Movement/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+    Jump jump;
+    
+    // Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+        jump = GetComponent<Jump>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
