db7eb2f958f8fd7566207835ddd76564a456c4b3
GiveMeButter <33163518+GiveMeButter@users.noreply.github.com>
2018-04-18 11:01:46 -0600

    level loader

--- Unity Project/Claws and Honor Unity Project/Assets/LevelLoader.cs
+++ Unity Project/Claws and Honor Unity Project/Assets/LevelLoader.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
 
 	public void LoadLevel (int sceneIndex)
     {
@@ -14,9 +18,12 @@ public class LevelLoader : MonoBehaviour {
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
 
             yield return null;
         }
